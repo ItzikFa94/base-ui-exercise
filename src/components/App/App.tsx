bd9ec75c9af35d7e864b23c6b583ceb59e6b6380
nitayneeman <nitayneeman@users.noreply.github.com>
2021-04-05 09:16:05 +0300

    feat: add templates to complete

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,17 @@
+import { PanelHeader } from '@wix/wix-base-ui';
 import React from 'react';
+import classes from './App.scss';
 
 interface AppProps {}
 
 class App extends React.Component<AppProps> {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className={classes.root}>
+        <PanelHeader className={classes.header}>Button Settings</PanelHeader>
+        Complete me...
+      </div>
+    );
   }
 }
 
